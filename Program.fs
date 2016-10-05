82e390e84261cd72baace8951f0680dea70ee054
YB <Youenn-Bouglouan>
2016-10-05 23:32:38 +0200

    added folder creation for app data (tested on Linux)

--- Program.fs
+++ Program.fs
@@ -4,16 +4,25 @@ module Program =
 
     open System
     open GpsData
+    open System.IO
 
     [<EntryPoint>]
     let main argv = 
 
         // where to save the file
-        let filename = "gpsData_"
-        let savePath = @"c:\DATA\DEV\FSharp\MyApp\SavedData\"
-        let timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")
-        let fullFilename = sprintf "%s%s.json" filename timestamp
-        let fullFilenameWithPath = sprintf "%s%s" savePath fullFilename
+
+        let getFilename =
+            let filename = "gpsData_"
+            let savePath = "./SavedData/"
+            
+            if not <| Directory.Exists(savePath) then
+                printfn "Creating folder '%s'..." savePath
+                Directory.CreateDirectory(savePath) |> ignore
+                printfn "Folder '%s' created." filename
+
+            let timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")
+            let fullFilename = sprintf "%s%s.json" filename timestamp
+            sprintf "%s%s" savePath fullFilename
 
         let lines= [
             {Name="a"; Type= Bus};
@@ -21,6 +30,6 @@ module Program =
         ]
         
         // Save a json file with the gps data for the lines above
-        GpsData.saveGpsDataAsync fullFilenameWithPath lines |> Async.RunSynchronously
+        GpsData.saveGpsDataAsync getFilename lines |> Async.RunSynchronously
 
         0 // exit code
